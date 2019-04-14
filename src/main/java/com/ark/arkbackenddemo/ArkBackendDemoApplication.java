0a5380712a5186f0b215e055d31d22872ffcea27
krrathore1990 <karanr15.rathore@gmail.com>
2019-04-14 23:09:25 +0530

    added src

---
+++ src/main/java/com/ark/arkbackenddemo/ArkBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendDemoApplication.class, args);
+	}
+
+}
